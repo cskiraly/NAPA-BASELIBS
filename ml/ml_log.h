6695360e42570f69bd0e2cd0cacf2dd03411de75
Csaba Kiraly <kiraly@disi.unitn.it>
2011-10-24 12:03:00 +0200

    ML: releasing ML under LGPL

--- ml/ml_log.h
+++ ml/ml_log.h
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2009 NEC Europe Ltd. All Rights Reserved.
+ * Authors: Kristian Beckers  <beckers@nw.neclab.eu>
+ *          Sebastian Kiesel  <kiesel@nw.neclab.eu>
+ *
+ * This file is part of the Messaging Library.
+ *
+ * The Messaging Library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * The Messaging Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Messaging Library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef _ML_LOG_H
 #define _ML_LOG_H
 
--- ml/util/stun.c
+++ ml/util/stun.c
@@ -1,35 +1,23 @@
 /*
- *          Policy Management
- *
- *          NEC Europe Ltd. PROPRIETARY INFORMATION
- *
- * This software is supplied under the terms of a license agreement
- * or nondisclosure agreement with NEC Europe Ltd. and may not be
- * copied or disclosed except in accordance with the terms of that
- * agreement.
- *
- *      Copyright (c) 2009 NEC Europe Ltd. All Rights Reserved.
- *
+ * Copyright (c) 2009 NEC Europe Ltd. All Rights Reserved.
  * Authors: Kristian Beckers  <beckers@nw.neclab.eu>
- *    Sebastian Kiesel  <kiesel@nw.neclab.eu>
- *          
+ *          Sebastian Kiesel  <kiesel@nw.neclab.eu>
  *
- * NEC Europe Ltd. DISCLAIMS ALL WARRANTIES, EITHER EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS FOR A PARTICULAR PURPOSE AND THE WARRANTY AGAINST LATENT
- * DEFECTS, WITH RESPECT TO THE PROGRAM AND THE ACCOMPANYING
- * DOCUMENTATION.
+ * This file is part of the Messaging Library.
  *
- * No Liability For Consequential Damages IN NO EVENT SHALL NEC Europe
- * Ltd., NEC Corporation OR ANY OF ITS SUBSIDIARIES BE LIABLE FOR ANY
- * DAMAGES WHATSOEVER (INCLUDING, WITHOUT LIMITATION, DAMAGES FOR LOSS
- * OF BUSINESS PROFITS, BUSINESS INTERRUPTION, LOSS OF INFORMATION, OR
- * OTHER PECUNIARY LOSS AND INDIRECT, CONSEQUENTIAL, INCIDENTAL,
- * ECONOMIC OR PUNITIVE DAMAGES) ARISING OUT OF THE USE OF OR INABILITY
- * TO USE THIS PROGRAM, EVEN IF NEC Europe Ltd. HAS BEEN ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGES.
+ * The Messaging Library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- *     THIS HEADER MAY NOT BE EXTRACTED OR MODIFIED IN ANY WAY.
+ * The Messaging Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Messaging Library.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #include <assert.h>
