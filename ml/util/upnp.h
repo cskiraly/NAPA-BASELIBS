4cb8543042ed8708c85e443a85c3d5e2ca3d2f80
Martino Salvetti <the9ull@silix.org>
2012-04-17 16:27:24 +0200

    Support the linking of the lib miniupnpc

--- ml/util/upnp.h
+++ ml/util/upnp.h
@@ -4,7 +4,7 @@
 #define upnp_add_TCP_redir(p) upnp_add_redir(p, "TCP")
 #define upnp_add_UDP_redir(p) upnp_add_redir(p, "UDP")
 
-int upnp_init(void);
+int upnp_init();
 int upnp_add_redir(int port, const char *protocol);
 void upnp_rem_redir(int port);
 
